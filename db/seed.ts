5db00b6f941609676ca2320e6990b959a4433bb8
Steve Gomez <104510289+badkitten-bug@users.noreply.github.com>
2024-06-23 20:50:00 -0500

    chore: Update seed.ts to insert initial users

--- db/seed.ts
+++ db/seed.ts
@@ -1,16 +1,45 @@
-import { User, db } from "astro:db";
+import { Category, Todo, User, db } from "astro:db";
 
 export default async function seed() {
   await db.insert(User).values([
     {
-      id: "1",
-      email: "pepo@gmail.com",
-      username: "pepo",
+      id: "sof@fosx637",
+      email: "pepito@gmail.com",
+      username: "pepito",
     },
     {
-      id: "2",
-      email: "edson@gmail.com",
-      username: "edson",
+      id: "xx33@@ds",
+      email: "juan@gmail.com",
+      username: "juan",
+    },
+  ]);
+  await db.insert(Category).values([
+    {
+      id: "ppsfsf@",
+      label: "software",
+    },
+    {
+      id: "ooopd@d",
+      label: "marketing",
+    },
+  ]);
+
+  await db.insert(Todo).values([
+    {
+      id: "ggtt2@",
+      title: "Crear funcion de fetch",
+      description: "Crear una funcion que adapte las fechas",
+      user_id: "sof@fosx637",
+      category_id: "ppsfsf@",
+    },
+  ]);
+  await db.insert(Todo).values([
+    {
+      id: "wweep@",
+      title: "Crear anuncio",
+      description: "Crear anuncio del producto",
+      user_id: "xx33@@ds",
+      category_id: "ooopd@d",
     },
   ]);
 }
